61f3839dfe88750053c168c1d22762aefbc87ad3
username <wecp-dummy@gmail.com>
2024-12-10 06:45:51 +0000

    wecp initial commit

--- backend/src/main/java/com/wecp/progressive/service/impl/TeamServiceImplArraylist.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/TeamServiceImplArraylist.java
@@ -1,33 +1,35 @@
 package com.wecp.progressive.service.impl;
- 
+
 import com.wecp.progressive.entity.Team;
 import com.wecp.progressive.service.TeamService;
- 
+import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
- 
+
+@Service
 public class TeamServiceImplArraylist  implements TeamService {
- 
+
     private static List<Team> teamList = new ArrayList<>();
     @Override
     public List<Team> getAllTeams() {
         return teamList;
     }
- 
+
     @Override
     public int addTeam(Team team) {
         teamList.add(team);
         return teamList.size();
     }
- 
+
     @Override
     public List<Team> getAllTeamsSortedByName() {
         List<Team> sortedTeam = teamList;
         sortedTeam.sort(Comparator.comparing(Team::getTeamName));
         return sortedTeam;
     }
- 
+
     @Override
     public void emptyArrayList() {
         teamList = new ArrayList<>();
--- backend/src/main/java/com/wecp/progressive/service/impl/TeamServiceImplJpa.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/TeamServiceImplJpa.java
@@ -1,5 +1,42 @@
 package com.wecp.progressive.service.impl;
 
-public class TeamServiceImplJpa  {
+import com.wecp.progressive.entity.Team;
+import com.wecp.progressive.service.TeamService;
+import org.springframework.stereotype.Service;
 
+import java.sql.SQLException;
+import java.util.List;
+
+@Service
+public class TeamServiceImplJpa  implements TeamService {
+
+    @Override
+    public List<Team> getAllTeams() throws SQLException {
+        return List.of();
+    }
+
+    @Override
+    public int addTeam(Team team) throws SQLException {
+        return -1;
+    }
+
+    @Override
+    public List<Team> getAllTeamsSortedByName() throws SQLException {
+        return List.of();
+    }
+
+    @Override
+    public Team getTeamById(int teamId) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public void updateTeam(Team team) throws SQLException {
+
+    }
+
+    @Override
+    public void deleteTeam(int teamId) throws SQLException {
+
+    }
 }
